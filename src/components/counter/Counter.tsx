7b1a12681a99b85e6e768b45c5e32db0e21b0a9e
Andriikryl <andreyk557@gmail.com>
2023-09-11 12:36:45 +0300

    add redux store counter

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+"use client";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  reset,
+  incrementByAmount,
+} from "../../features/counterSlice";
+import { useState } from "react";
+import { RootState } from "../../store/store";
+import { AnimatedNumber } from "../animateValue/AnimatedNumber";
 
-export default function Counter() {
-  return <div>Counter</div>;
-}
+const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState(0);
+
+  const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
+  return (
+    <section>
+      <AnimatedNumber value={count} />
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(parseInt(e.target.value))}
+      />
+
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
+      </div>
+    </section>
+  );
+};
+export default Counter;
