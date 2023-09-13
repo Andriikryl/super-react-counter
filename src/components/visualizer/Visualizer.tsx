2732516e5f9831efe2d5b302dd724db82163de3b
Andriikryl <andreyk557@gmail.com>
2023-09-13 13:51:03 +0300

    add sort array button and random sort array button

--- src/components/visualizer/Visualizer.tsx
+++ src/components/visualizer/Visualizer.tsx
@@ -2,27 +2,54 @@
 import React from "react";
 import style from "./style.module.css";
 import { Container } from "../container/Container";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/store/store";
+import { randomSortNumberList, sortNumberList } from "@/features/listSlice";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Visualizer() {
+  const dispatch = useDispatch();
   const numberList = useSelector((state: RootState) => state.list.numberList);
   const maxNumber = Math.max(...numberList);
   const scaleFactor = maxNumber > 0 ? 350 / maxNumber : 1;
+  const handleSort = () => {
+    dispatch(sortNumberList());
+  };
+  const handleRandomSort = () => {
+    dispatch(randomSortNumberList());
+  };
   return (
     <section className={style.visualizer}>
       <Container>
         <h4 className={style.visualizer__title}>Visualizer</h4>
         <div className={style.visualizer__box}>
-          {numberList.map((element, index) => (
-            <div
-              className={style.element}
-              key={index}
-              style={{ height: element * scaleFactor + "px" }}
-            >
-              <span className={style.element__number}>{element}</span>
-            </div>
-          ))}
+          <AnimatePresence>
+            {numberList.map((element, index) => (
+              <motion.div
+                className={style.element}
+                key={index}
+                style={{ height: element * scaleFactor + "px" }}
+                layout
+                initial={{ opacity: 0, x: 200 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{
+                  opacity: 0,
+                  x: -200,
+                }}
+                transition={{ opacity: { duration: 0.7 } }}
+              >
+                <span className={style.element__number}>{element}</span>
+              </motion.div>
+            ))}
+          </AnimatePresence>
+        </div>
+        <div className={style.button__group}>
+          <button className={style.button} onClick={handleSort}>
+            Sort Array
+          </button>
+          <button className={style.button} onClick={handleRandomSort}>
+            random sort Array
+          </button>
         </div>
       </Container>
     </section>
--- src/features/listSlice.ts
+++ src/features/listSlice.ts
@@ -23,9 +23,35 @@ const listSlice = createSlice({
     deleteAllNumbers: (state) => {
       state.numberList = [];
     },
+    sortNumberList: (state) => {
+      state.numberList.sort((a, b) => a - b);
+    },
+    randomSortNumberList: (state) => {
+      const shuffledList = [...state.numberList];
+      for (let i = shuffledList.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
+      }
+      state.numberList = shuffledList;
+    },
+    bubbleSortNumberList: (state) => {
+      const { numberList } = state;
+      const len = numberList.length;
+      let swapped;
+      
+      do {
+        swapped = false;
+        for (let i = 0; i < len - 1; i++) {
+          if (numberList[i] > numberList[i + 1]) {
+            [numberList[i], numberList[i + 1]] = [numberList[i + 1], numberList[i]];
+            swapped = true;
+          }
+        }
+      } while (swapped);
+    },
   },
 });
 
-export const { addNumber, deleteNumber, deleteAllNumbers } = listSlice.actions;
+export const { addNumber, deleteNumber, deleteAllNumbers, sortNumberList, randomSortNumberList, bubbleSortNumberList } = listSlice.actions;
 
 export default listSlice.reducer;
